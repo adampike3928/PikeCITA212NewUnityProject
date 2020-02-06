5642707ff34eea00a9e92d9e9f44882a660ce209
adampike334 <adampikespam@gmail.com>
2020-02-06 14:47:56 -0500

    Added varables intmax and intmin

---
+++ New Unity Project/Assets/HellowWorld.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class HellowWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
